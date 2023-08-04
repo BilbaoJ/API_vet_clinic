83cd2cfd3dbb874d72ad0c05c532a54ae60220bf
BilbaoJ <jessicabilbao02@gmail.com>
2023-08-04 15:37:41 -0500

    Add MongoDB service

---
+++ src/services/mongodb.service.js
@@ -0,0 +1,46 @@
+const { MongoClient } = require("mongodb");
+const uri = process.env.URI_MONGODB;
+const client = new MongoClient(uri);
+
+const conectDB = async () => {
+  await client.connect();
+  let DB = client.db(process.env.DB_MONGODB);
+  return DB;
+}
+
+const readDocuments = async (collectionName) => {
+  let db = await conectDB();
+  let collection = db.collection(collectionName);
+  return collection.find().toArray();
+}
+
+const readDocument = async (collectionName, filter) => {
+  let db = await conectDB();
+  let collection = db.collection(collectionName);
+  return collection.findOne(filter);
+}
+
+const createDocument = async (collectionName, info)=>{
+  let db = await conectDB();
+  let collection = db.collection(collectionName);
+  return await collection.insertOne(info);
+}
+
+const deleteDocument = async (collectionName, filter)=>{
+  let db = await conectDB();
+  let collection = db.collection(collectionName);
+  return await collection.deleteOne(filter);
+}
+
+const modifyDocument = async (collectionName, filter, newDocument)=>{
+  let db = await conectDB();
+  let coleccion = db.collection(collectionName);
+  return await coleccion.replaceOne(filter, newDocument);
+}
+
+module.exports = {
+  readDocuments,
+  readDocument,
+  createDocument,
+  deleteDocument,
+  modifyDocument};
